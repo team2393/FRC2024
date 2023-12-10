c1b370d17b851046059498cb93a39bc1b9c278e8
Kay Kasemir <kay.kasemir@gmail.com>
2023-12-10 08:22:41 -0500

    Update RobotMap.java

--- src/main/java/frc/robot/RobotMap.java
+++ src/main/java/frc/robot/RobotMap.java
@@ -7,5 +7,14 @@ package frc.robot;
 /** Robot (Hardware) Map: What's connected where? */
 public class RobotMap
 {
+  // TODO Power Distribution Info
+  // Add someting similar to the comment in
+  // https://github.com/team2393/FRC2022/blob/main/src/main/java/frc/robot/RobotMap.java
+  // which shows the outputs of the power distribution panel
+  // and how they are used: What motor/device/.., what type of fuse.
+  // See https://docs.wpilib.org/en/latest/docs/controls-overviews/control-system-hardware.html
+  // for the potential types of power distribution panels.
+
+  // CAN IDs
   public static final int FRONT_LEFT_DRIVE = 1;
 }
