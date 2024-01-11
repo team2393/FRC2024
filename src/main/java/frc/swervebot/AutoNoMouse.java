d926f0b384643e180299815e0530a62b13ac6cae
Kay <kay.kasemir@gmail.com>
2024-01-10 19:44:45 -0500

    Fix 'circle' path start

--- src/main/java/frc/swervebot/AutoNoMouse.java
+++ src/main/java/frc/swervebot/AutoNoMouse.java
@@ -47,9 +47,9 @@ public class AutoNoMouse
     }
 
     {
-      autos.add(new SequenceWithStart("Circle", 1.66, 4.47, 0,
+      autos.add(new SequenceWithStart("Circle", 1.91, 2.245, 0,
                                       new VariableWaitCommand(),
-                                      new SelectAbsoluteTrajectoryCommand(drivetrain, 0, 0, 0),
+                                      new SelectAbsoluteTrajectoryCommand(drivetrain, 1.91, 2.245, 0),
                                       followPathWeaver(drivetrain, "Circle", 0.0)));
     }
 
