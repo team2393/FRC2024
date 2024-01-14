9e0a3e5aab159352135ae473472b6f8d804731b9
kasparwinston <winstonkr@icloud.com>
2024-01-14 14:26:47 -0500

    Fixed Rainbow LED overextending its time

--- src/main/java/frc/led/Rainbow.java
+++ src/main/java/frc/led/Rainbow.java
@@ -9,136 +9,93 @@ import edu.wpi.first.wpilibj2.command.Command;
 public class Rainbow extends Command
 {
   private final LEDRing ring;
+  private int i = 0;
+  private final int TARGET_INCREMENT = 5;
+  private int target = TARGET_INCREMENT;
+  private int stage = 0;
 
   public Rainbow(LEDRing ring)
   {
     this.ring = ring;
     addRequirements(ring);
   }
-
+  
   @Override
   public boolean runsWhenDisabled()
   {
     return true;
   }
-
+  
   @Override
   public void execute()
   {
-    ring.clear();
-
-    // red -> orange -> yellow
-    for (int i = 0; i < 256; i++)
+    if (stage == 5)
     {
-      for (int j = 0; j < ring.N; j++)
-      {
-        ring.buffer.setRGB(j, 255, i, 0);
-        ring.led.setData(ring.buffer);
-      }
-
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
-      }
+      stage = 0;
     }
 
-    // yellow -> green
-    for (int i = 255; i > 0; i--)
+    // goes through colors in small chunks and preserves its current position in execute()
+    for (i = i; i < target; i++)
     {
+      // move on to next stage if finished with one RGB value
+      if (i >= 255)
+      {
+        // reset if cycle is complete
+        if (stage == 5)
+        {
+          // stage = 0;
+        }
+        else
+        {
+          stage++;
+          i = 0;
+          target = TARGET_INCREMENT;
+        }
+      }
+
       for (int j = 0; j < ring.N; j++)
       {
-        ring.buffer.setRGB(j, i, 255, 0);
-        ring.led.setData(ring.buffer);
-      }
+        switch (stage)
+        {
+          // red -> orange -> yellow
+          case 0:
+            ring.buffer.setRGB(j, 255, i, 0);
+            ring.led.setData(ring.buffer);
+            break;
 
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
+          // yellow -> green
+          case 1:
+            ring.buffer.setRGB(j, 255 - i, 255, 0);
+            ring.led.setData(ring.buffer);
+            break;
+          
+          // green -> teal
+          case 2:
+            ring.buffer.setRGB(j, 0, 255, i);
+            ring.led.setData(ring.buffer);
+            break;
+
+          // teal -> blue
+          case 3:
+            ring.buffer.setRGB(j, 0, 255 - i, 255);
+            ring.led.setData(ring.buffer);
+            break;
+
+          // blue -> purple
+          case 4:
+            ring.buffer.setRGB(j, i, 0, 255);
+            ring.led.setData(ring.buffer);
+            break;
+
+          // purple -> red
+          case 5:
+            ring.buffer.setRGB(j, 255, 0, 255 - i);
+            ring.led.setData(ring.buffer);
+            break;
+        }
       }
     }
-
-    // green -> teal
-    for (int i = 0; i < 256; i++)
-    {
-      for (int j = 0; j < ring.N; j++)
-      {
-        ring.buffer.setRGB(j, 0, 255, i);
-        ring.led.setData(ring.buffer);
-      }
-
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
-      }
-    }
-
-    // teal -> blue
-    for (int i = 255; i > 0; i--)
-    {
-      for (int j = 0; j < ring.N; j++)
-      {
-        ring.buffer.setRGB(j, 0, i, 255);
-        ring.led.setData(ring.buffer);
-      }
-
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
-      }
-    }
-
-    // blue -> purple
-    for (int i = 0; i < 256; i++)
-    {
-      for (int j = 0; j < ring.N; j++)
-      {
-        ring.buffer.setRGB(j, i, 0, 255);
-        ring.led.setData(ring.buffer);
-      }
-
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
-      }
-    }
-
-    // purple -> red
-    for (int i = 255; i > 1; i--)
-    {
-      for (int j = 0; j < ring.N; j++)
-      {
-        ring.buffer.setRGB(j, 255, 0, i);
-        ring.led.setData(ring.buffer);
-      }
-
-      try
-      {
-        Thread.sleep(3);
-      }
-      catch (Exception e)
-      {
-        // do nothing
-      }
-    }
+    
+    target += TARGET_INCREMENT;
   }  
 }
