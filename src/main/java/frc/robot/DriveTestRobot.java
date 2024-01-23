dba2dadb899c751b4096a5232fe8167adc48a2af
kasemir <ky9@ornl.gov>
2024-01-23 10:31:51 -0500

    Driver

---
+++ src/main/java/frc/robot/DriveTestRobot.java
@@ -0,0 +1,15 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import frc.swervelib.DriverDemoRobot;
+
+/** Robot for testing a single Driver */
+public class DriveTestRobot extends DriverDemoRobot
+{
+    public DriveTestRobot()
+    {
+        super(new Driver(0, RobotMap.FRONT_LEFT_DRIVE));
+    }
+}
