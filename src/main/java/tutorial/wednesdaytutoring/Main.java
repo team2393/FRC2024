18489409620061b8a30315a25d9df314bfc013b4
kasparwinston <winstonkr@icloud.com>
2024-01-24 12:27:24 -0500

    Lesson 1

--- src/main/java/tutorial/wednesdaytutoring/Main.java
+++ src/main/java/tutorial/wednesdaytutoring/Main.java
@@ -2,5 +2,20 @@ package tutorial.wednesdaytutoring;
 
 public class Main
 {
-    // nothing yet
+    public static void main(String[] args)
+    {
+        int number = 10;
+        char character = 'l';
+        String string = "Hello World!";
+
+        System.out.println(number);
+        System.out.println(character);
+        System.out.println(string);
+
+        System.out.println("Welcome to my bank");
+        System.out.println("Option 1: loan");
+        System.out.println("Option 1: loan");
+        System.out.println("Option 1: loan");
+        System.out.println("Option 1: loan");
+    }
 }
