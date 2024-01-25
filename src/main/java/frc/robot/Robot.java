97f2ef4963992c6687e556dcf2c92b32b3a60ca5
Kay <kay.kasemir@gmail.com>
2024-01-24 19:02:06 -0500

    Start robot drivetrain

---
+++ src/main/java/frc/robot/Robot.java
@@ -0,0 +1,66 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
+import frc.swervelib.RelativeSwerveCommand;
+import frc.swervelib.SwerveDrivetrain;
+import frc.swervelib.SwerveOI;
+import frc.tools.AutoTools;
+import frc.tools.CommandRobotBase;
+
+/** The robot */
+public class Robot extends CommandRobotBase
+{
+  private final RobotDrivetrain drivetrain = new RobotDrivetrain();
+  private final Command relswerve = new RelativeSwerveCommand(drivetrain);
+  
+  private final SendableChooser<Command> autos = new SendableChooser<>();
+
+  @Override
+  public void robotInit()
+  {
+    super.robotInit();
+
+    SwerveDrivetrain.MAX_METERS_PER_SEC = 1.5;
+    SwerveDrivetrain.MAX_ROTATION_DEG_PER_SEC = 45;
+    SwerveOI.forward_slew = new SlewRateLimiter(1.5);
+    SwerveOI.side_slew = new SlewRateLimiter(1.5);
+    SwerveOI.rotation_slew = new SlewRateLimiter(90);
+
+    SwerveOI.reset();
+
+    autos.setDefaultOption("Nothing", new PrintCommand("Do nothing"));
+
+    // for (Command auto : AutoNoMouse.createAutoCommands(drivetrain))
+    //   autos.addOption(auto.getName(), auto);
+    SmartDashboard.putData(autos);
+  }
+  
+  @Override
+  public void disabledPeriodic()
+  {
+    AutoTools.indicateStart(drivetrain, autos.getSelected());
+  }  
+
+  @Override
+  public void teleopInit()
+  {
+    // Bind buttons to commands
+    drivetrain.setDefaultCommand(relswerve);
+
+    // Start relative mode
+    relswerve.schedule();
+  }
+
+  @Override
+  public void autonomousInit()
+  {
+    autos.getSelected().schedule();
+  }
+}
---
+++ src/main/java/frc/robot/RobotDrivetrain.java
@@ -0,0 +1,48 @@
+// Copyright (c) FIRST Team 2393 and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+package frc.robot;
+
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import frc.swervelib.SwerveDrivetrain;
+import frc.swervelib.SwerveModule;
+
+/** Robot Drivetrain */
+public class RobotDrivetrain extends SwerveDrivetrain
+{
+// TODO  private final PigeonIMU gyro = new PigeonIMU(0);
+
+  public RobotDrivetrain()
+  {
+    super(0.393,
+          0.416,
+          new SwerveModule(new Rotator(0, RobotMap.FRONT_LEFT_ROTATE, RobotMap.FRONT_LEFT_ANGLE, 0.0),
+                            new Driver (0, RobotMap.FRONT_LEFT_DRIVE)),
+          new SwerveModule(new Rotator(1, RobotMap.FRONT_RIGHT_ROTATE, RobotMap.FRONT_RIGHT_ANGLE, 0.0),
+                            new Driver (1, RobotMap.FRONT_RIGHT_DRIVE)),
+          new SwerveModule(new Rotator(2, RobotMap.BACK_RIGHT_ROTATE, RobotMap.BACK_RIGHT_ANGLE, 0.0),
+                            new Driver (2, RobotMap.BACK_RIGHT_DRIVE)),
+          new SwerveModule(new Rotator(3, RobotMap.BACK_LEFT_ROTATE, RobotMap.BACK_LEFT_ANGLE, 0.0),
+                            new Driver (3, RobotMap.BACK_LEFT_DRIVE))
+          );
+  }
+
+  public double getRawHeading()
+  {
+    return 0.0;
+ // TODO   return gyro.getFusedHeading();
+  }
+
+  public double getPitch()
+  {
+    return 0.0;
+    // TODO  return -gyro.getPitch();
+  }
+
+  public double getRoll()
+  {
+    return 0.0;
+    // TODO  return -gyro.getRoll();
+  }
+}
