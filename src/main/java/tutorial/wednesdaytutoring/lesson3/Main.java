36d8863517e1284931b87d8f1498efec4fa2792e
kasparwinston <winstonkr@icloud.com>
2024-01-31 16:38:06 -0500

    Lessons 2 + 3 and putting them in their own directories

---
+++ src/main/java/tutorial/wednesdaytutoring/lesson3/Main.java
@@ -0,0 +1,30 @@
+package tutorial.wednesdaytutoring.lesson3;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int myNumber = 6;
+
+        if (myNumber == 5)
+        {
+            System.out.println("It works!");
+        }
+        else if (myNumber >= 4)
+        {
+            System.out.println("It also works!");
+        }
+        else if (myNumber == 3)
+        {
+            System.out.println("It also works!");
+        }
+        else if (myNumber == 3)
+        {
+            System.out.println("It also works!");
+        }
+        else if (myNumber == 3)
+        {
+            System.out.println("It also works!");
+        }
+    }
+}
