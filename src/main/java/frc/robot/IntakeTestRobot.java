cb241ec371e9f18e988adadfb042a07071d350c7
Kay <kay.kasemir@gmail.com>
2024-02-21 20:06:27 -0500

    Pneumatic config for analog sensor

--- src/main/java/frc/robot/IntakeTestRobot.java
+++ src/main/java/frc/robot/IntakeTestRobot.java
@@ -3,18 +3,21 @@
 // the WPILib BSD license file in the root directory of this project.
 package frc.robot;
 
+import edu.wpi.first.wpilibj.PneumaticHub;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.tools.CommandRobotBase;
 
 /** Robot for testing the intake */
 public class IntakeTestRobot extends CommandRobotBase
 {
+  private final PneumaticHub hub = new PneumaticHub();
   private final Intake intake = new Intake();
   private final Feeder feeder = new Feeder();
   private final Command open_intake = new OpenIntakeCommand(intake, feeder);
   
   public IntakeTestRobot()
   {
+    hub.enableCompressorAnalog(85, 120);
     OperatorInterface.toggleIntake().toggleOnTrue(open_intake);
   }
 }
